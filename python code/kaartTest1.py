dceeb9e0c621ec549d569bc9795c999e3d6f9778
BrendanMesters <brendanmesters@gmail.com>
2017-11-06 14:21:44 +0100

    python code

--- python code/kaartTest1.py
+++ python code/kaartTest1.py
@@ -7,6 +7,6 @@ foo = kaart.Kaart()
 for i in range(10):
 	print(i)
 	foo.matrixRC[i][6] = 1
-print(foo.how_far_line(6, 0, math.pi*0.5))
+print(foo.how_far_line(6, 0, math.pi*.5))
 foo.matrixRC[6][0]=0
 print(foo)
