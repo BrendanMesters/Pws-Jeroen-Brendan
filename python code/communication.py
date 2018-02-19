cad9f51d37f7afce49c6513d7cadad4fa71ed1e1
Brendan Mesters <brendanmesters@gmail.com>
2018-02-19 10:56:02 +0000

    making a function to construct a list of measurments I've recieved from the sonar/radar

--- python code/communication.py
+++ python code/communication.py
@@ -1,26 +1,58 @@
 import serial
-ser = serial.Serial("/dev/tty.usbmodem411")
+import time
+ser = serial.Serial("/dev/ttyACM0")
 
 def write(arg, answer = False):
-        assert type(arg) == str
-        retval = ser.write(str.encode(arg))
-        if answer == True:
-            return retval
+    assert type(arg) == str
+    retval = ser.write(str.encode(arg))
+    if answer == True:
+        return retval
 
 
 def read(fullString = False, wait = False):
-        retval = None
-        if wait:
-            while ser.inWaiting() == 0:
-                pass
+    retval = None
+    if wait:
+        while ser.inWaiting() == 0:
+            pass
+        retval = ser.readline()
+    else:
+        if ser.inWaiting() != 0:
             retval = ser.readline()
+    if fullString:
+        return retval
+    elif retval != None:
+        return retval.strip()
+
+
+def readData():
+    retval = []
+    T1 = 0
+    T2 = 0
+    timeOut = False
+    Finished = False
+    timeOutTime = 10 #Time that has to pass befor we call a timeout
+    while Finished == False and timeOut == False:
+        foo = None
+        ## TEST CODE!!!!1
+#        foo = raw_input('input ')
+        foo = read()
+        if foo == 'OK':
+            Finished = True
+        elif foo == None:
+            if T1 == 0:
+                T1 = time.time()
+            else:
+                T2 = time.time()
+                if T2-T1 >= timeOutTime:
+                    timeOut = True
         else:
-            if ser.inWaiting() != 0:
-                retval = ser.readline()
-        if fullString:
-            return retval
-        else:
-            return retval.strip()
+            T1, T2 = 0, 0
+            assert foo.isdigit(), 'Unexpected serial readout'
+            retval.append(int(foo))
+    if timeOut:
+        assert False, 'Timeout during readData()'
+    return retval
+
 
 def main_loop():
     while True:
@@ -28,6 +60,6 @@ def main_loop():
         if arg == 'Read' or arg == 'read':
             foo = read()
             if foo != None:
-                print(read())
+                print(foo)
         elif arg == 'Write' or arg == 'write':
             write(raw_input("Wat wil je schrijven").encode('utf-8'))
